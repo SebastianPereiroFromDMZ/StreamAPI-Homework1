3432460ec03a59dcfa1b3bad5500640a2264a35b
Kost Kuzmin <Kuzmin_konstantin@bk.ru>
2022-12-14 13:57:01 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        Collections.sort(intList);
+
+        for (int x : intList) {
+            if (x > 0) {
+                if (x % 2 == 0) {
+                    System.out.println(x);
+                }
+            }
+        }
+    }
+}
